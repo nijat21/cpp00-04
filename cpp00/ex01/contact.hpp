04c29d5d86f046ac660fa1c432b9e2d81baee26b
Nijat Ismayilov <nijat.ismayilov@outlook.com>
2026-04-03 00:45:03 +0100

    structure

--- cpp00/ex01/contact.hpp
+++ cpp00/ex01/contact.hpp
@@ -1,4 +1,31 @@
 #ifndef CONTACT_H
 #define CONTACT_H
 
+#include <string>
+
+struct ContactInput {
+    std::string first_name;
+    std::string last_name;
+    std::string nickname;
+    std::string phone_number;
+    std::string darkest_secret;
+};
+
+class Contact {
+  private:
+    std::string first_name;
+    std::string last_name;
+    std::string nickname;
+    std::string phone_number;
+    std::string darkest_secret;
+
+  public:
+    void set_first_name(std::string str);
+    void set_last_name(std::string str);
+    void set_nickname(std::string str);
+    void set_phone_number(std::string str);
+    void set_darkest_secret(std::string str);
+    void add(ContactInput contact);
+}
+
 #endif
---
+++ cpp00/ex01/main.cpp
@@ -0,0 +1,34 @@
+#include "phonebook.hpp"
+#include "contact.hpp"
+
+ContactInfo fill_struct()
+{
+	while(1)
+	{
+			}
+}
+
+
+int main(int ac, char **av)
+{
+	if(av != 1) 
+		return 1;
+	while(1)
+	{
+		// if ADD
+		// 	prompt for:
+		// 		first_name
+		// 		last_name
+		// 		nickname
+		// 		phone
+		// 		secret
+		// 	if any empty:
+		// 		don't add the class
+		// 	else:
+		// 		add to phonebook
+		// if SEARCH
+		// 	search and display result
+		// if EXIT, return
+		// else continue;
+	}
+}
